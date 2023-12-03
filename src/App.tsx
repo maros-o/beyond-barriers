68743e707263df13b8a7405ef4d8a2d5dbfbe627
Maros Meciar <meciar@atlasgroup.cz>
2023-12-03 12:59:43 +0100

    auto

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const App = () => (
           <div className="titlebg w-[296px] left-[165px]"></div>
         </div>
         <ul className="flex-wrap-cc gap-20 mt-4">
-          <Merch price={299} src={"merchMug"} title={"Hrníček"} />
+          <Merch price={299} src={"merchMugGray"} title={"Hrníček"} />
           <Merch price={499} src={"teamJersey"} title={"Týmový dres"} />
           <Merch
             price={1799}
